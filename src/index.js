2ed9b10e004c232fa366da40fcf370e3a1a1a558
Yoni Weisbrod <yoni@artsetters.com>
2016-12-02 19:33:21 +0200

    commit index.js

--- src/index.js
+++ src/index.js
@@ -7,10 +7,26 @@ require("../styles/application.scss");
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.js';
-import { Router, Route, browserHistory } from 'react-router';
-ReactDOM.render((
-	<Router history={ browserHistory }>
-		<Route path="/" component={ App } />
-	</Router>
+import { Router, Route, BrowserRouter } from 'react-router';
+import { UsersStore, TodosStore } from './models/stores';
+import { Provider } from 'mobx-react';
+import Todo from './models/Todo';
+import User from './models/User';
 
+const usersStore = new UsersStore();
+const gemma = usersStore.addUser('Gemma'); 
+const alice = usersStore.addUser('Alice');
+
+const todosStore = new TodosStore();
+todosStore.addTodo(new Todo('Go hunt for gems'), gemma);
+todosStore.addTodo(new Todo('Find a job'), alice);
+
+const allStores = {usersStore, todosStore}
+
+ReactDOM.render((
+  <Provider {...allStores}>
+  	<BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 ), document.getElementById('react-root'));
