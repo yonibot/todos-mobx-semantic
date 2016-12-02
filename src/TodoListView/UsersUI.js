1ecda60913a362423055047f011fff0cbf71adac
Yoni Weisbrod <yoni@artsetters.com>
2016-12-02 19:34:54 +0200

    add link to users ui and provider

--- src/TodoListView/UsersUI.js
+++ src/TodoListView/UsersUI.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
+import { inject, observer } from 'mobx-react';
+import { Link } from 'react-router';
 
+@inject('usersStore') @observer 
 class UsersUI extends Component {
 	render() {
 		return (
 			<div>
 			{ this.props.usersStore.users.map(u=> (
-					<h4 key={u.username}>User: {u.username}</h4>
+					<Link key={u.username} to={`/users/${u.username}`}><h4>User: {u.username}</h4></Link>
 				))}
+			<Link to='/users/new'>Add User</Link>
 			<br />
 			</div>
 		)
